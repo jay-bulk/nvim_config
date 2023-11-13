a5c08cdc27b5baa42c92dcb5708a1ae31e3b3319
jay-bulk <jay.bulk@proton.me>
2023-11-12 21:45:32 -0700

    fix: correct community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,24 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.typescript" },
-  -- { import = "astrocommunity.pack.javascript" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    { 
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
 }
